31d84c4f431994e4dcc04b556cdd35bafa75f8fa
Barb King <barbgross@aol.com>
2017-06-21 17:02:24 -0500

    plant modal

--- server/public/assets/scripts/controllers/plannerController.js
+++ server/public/assets/scripts/controllers/plannerController.js
@@ -1,4 +1,65 @@
-myApp.controller('PlannerController', ['$http', '$location', function($http, $location) {
+myApp.controller('PlannerController', ['$http', '$location', '$uibModal', '$log', 'seedService', function($http, $location, $uibModal, $log, seedService ) {
   var vm = this;
 
+  // create the list of seeds
+
+
+
+  vm.open = function ( size, parentSelector ) {
+    var parentElem = parentSelector ?
+      angular.element($document[0].querySelector('.add-plant-modal' + parentSelector)) : undefined;
+    var modalInstance = $uibModal.open({
+      animation: vm.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'addPlantModalContent.html',
+      controller: 'addPlantModalInstanceCtrl',
+      controllerAs: 'apmic',
+      size: size,
+      appendTo: parentElem,
+      resolve: {
+
+      }
+    }); // end modalInstance
+  }; // end newActivity
+
+}]);
+
+
+myApp.controller( 'addPlantModalInstanceCtrl', [ '$uibModalInstance', '$uibModal', '$log', 'seedService', function ( $uibModalInstance, $uibModal, $log, seedService ) {
+  var vm = this;
+
+  vm.inventory = seedService.inventory;
+  console.log ('apmic controller inventory',vm.inventory);
+
+  vm.addPlant = function(){
+    var itemToSend = {
+      crop: vm.crop,
+      location: vm.location,
+      date_planted: vm.date_planted,
+      quantity: vm.quantity,
+      area_sqft: vm.area_sqft,
+      notes: vm.notes,
+    };
+    //FILESTACK IMG URL GOES HERE
+    console.log(itemToSend);
+
+    $uibModalInstance.close();
+  };//end add Item
+
+
+  vm.clearPlantInputs = function (){
+    vm.crop = '';
+    vm.location = '';
+    vm.date_planted = '';
+    vm.quantity = '';
+    vm.area_sqft = '';
+    vm.notes = '';
+
+
+    $uibModalInstance.close();
+  };
+
+
+  vm.clearPlantInputs();
 }]);
