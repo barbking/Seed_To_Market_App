dba4984cc7720c4bb48994469ff22a896e3a9d08
Barb King <barbgross@aol.com>
2017-06-27 11:38:29 -0500

    added state selector in supplier modal

--- server/public/assets/scripts/controllers/suppliersController.js
+++ server/public/assets/scripts/controllers/suppliersController.js
@@ -29,6 +29,8 @@ myApp.controller('SuppliersController', ['$http','$location','$uibModal','$log',
 myApp.controller( 'addSupplierModalInstanceCtrl', [ '$uibModalInstance', '$uibModal', '$log', 'supplierService', function ( $uibModalInstance, $uibModal, $log, supplierService) {
   var vm = this;
 
+  vm.states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL","GA", "HI", "ID", "IL", "IN", "IA","KS","KY","LA","ME","MD","MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM","NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN","TX","UT","VT","VA","WA","WV","WI","WY"];
+
   vm.addNewSupplier = function(){
     var itemToSend = {
       name: vm.name,
