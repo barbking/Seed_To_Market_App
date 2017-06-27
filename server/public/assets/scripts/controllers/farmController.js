94e7c8dad9c99b12f5701cd14ea5898bfd472900
Barb King <barbgross@aol.com>
2017-06-27 11:30:47 -0500

    added state selector to farm profile inputs

--- server/public/assets/scripts/controllers/farmController.js
+++ server/public/assets/scripts/controllers/farmController.js
@@ -1,21 +1,19 @@
 myApp.controller( 'FarmController', [ '$http', '$filter', '$location', 'farmService', function( $http, $filter, $location, farmService ) {
   var vm = this;
 
+  vm.states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL","GA", "HI", "ID", "IL", "IN", "IA","KS","KY","LA","ME","MD","MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM","NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN","TX","UT","VT","VA","WA","WV","WI","WY"];
 
-vm.getFarmInfo = function(){
-  farmService.getFarm().then( function ( data ){
-    vm.farm = data;
-  });
-  console.log("vm.farm-->", vm.farm);
-};
+  vm.getFarmInfo = function(){
+    farmService.getFarm().then( function ( data ){
+      vm.farm = data;
+    });
+    console.log("vm.farm-->", vm.farm);
+  };
 
-
-    vm.saveFarm = function() {
-      console.log('in savefarm');
-      farmService.updateFarm(vm.farm);
-
-
-    };
+  vm.saveFarm = function() {
+    console.log('in savefarm');
+    farmService.updateFarm(vm.farm);
+  };
 
   vm.getFarmInfo();
 
