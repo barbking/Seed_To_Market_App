388bc699c96c4ba72dab51c0602d01a56207ac3f
Amy Venturino <aventuri@umich.edu>
2017-06-28 15:04:21 -0500

    seeds csv route functioning in postico

---
+++ server/public/assets/scripts/services/csv.js
@@ -0,0 +1,14 @@
+myApp.service('csvService', ['$http', '$location', function($http, $location) {
+  var self = this;
+
+self.seedsCsv = function (startDate, endDate){
+  $http({
+    method:'GET',
+    url: '/csv/exportSeeds/'+ startDate +'/'+ endDate
+  }).then(function success (response){
+    window.open ('/csv/exportSeeds/'+ startDate +'/'+ endDate);
+  });
+};
+
+
+}]);
--- server/routes/csv.js
+++ server/routes/csv.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var passport = require('passport');
 var path = require('path');
 var pg = require('pg');
+var csvtojson = require('csvtojson');
+var json2csv = require('json2csv');
 
 // set up config for the pool
 var config = {
@@ -15,42 +17,44 @@ var config = {
 // setup new pool
 var pool = new pg.Pool(config);
 
-// GET request .csv file
-router.get('/exportSeeds/:fromDate?/:toDate?', function(req, res, next) {
-
-      if (req.isAuthenticated()) {
+// GET request seeds .csv file
+router.get('/exportSeeds/:fromDate?/:toDate?', function (req, res, next) {
+  console.log('hit export seeds route');
+      // if (req.isAuthenticated()) {
         // connects to the pool
         pool.connect(function(err, connection, done) {
               if (err) {
-                res.sendStatus(400);
+                res.sendStatus( 403 );
               } else {
                 if (req.params.fromDate === undefined && req.params.toDate === undefined) {
-                  var jsonQueryAllSeeds = 'SELECT * FROM seeds WHERE user_id= $1',
-                    [req.user.user_id];
-                  db.query(jsonQueryAllSeeds, function(queryError, result) {
+                  var jsonQueryAllSeeds = 'SELECT * FROM seeds WHERE user_id= $1';
+                  connection.query(jsonQueryAllSeeds,[req.user.user_id], function(queryError, result) {
                     done();
                     if (queryError) {
                       res.sendStatus(500);
                     } else {
                       // converts query resutl to JSON
                       var jsonString = JSON.stringify(result.rows);
+                       console.log('result-->', result);
+                        console.log('result.rows -->', result.rows);
                       var json = JSON.parse(jsonString);
+                      console.log('json -->', json);
 
                       var opts = {
                         data: json,
                         feilds: ['seed_id', 'crop', 'date_created', 'variety', 'purchase_date', 'lot_number', 'quantity', 'out_of_stock', 'item_code', 'supplier_id', 'organic', 'untreated', 'non-gmo', 'seed_check_sources', 'receipt_url'],
                         quotes: ''
                       };
-
                       var result = json2csv(opts);
                       // sends csv file to client
                       res.attachment('Seed_Reports.csv');
                       res.status(200).send(result);
+                    }
+                  });
                     } else {
                       // query that selects all reports for a selected period of time
-                      var jsonQuerySeeds = 'SELECT * FROM seeds WHERE purchase_date >=$1 and purchase_date <=$2 and user_id= $3',
-                        [req.params.fromDate, req.params.toDate, req.user.user_id];
-                      db.query(jsonQuerySeeds, [fromDate, toDate], function(queryError, result) {
+                      var jsonQuerySeeds = 'SELECT * FROM seeds WHERE purchase_date >=$1 and purchase_date <=$2 and user_id= $3';
+                      connection.query(jsonQuerySeeds, [req.params.fromDate, req.params.toDate, 1], function(queryError, result) {
                         done();
                         if (queryError) {
                           res.sendStatus(500);
@@ -72,13 +76,12 @@ router.get('/exportSeeds/:fromDate?/:toDate?', function(req, res, next) {
                         } // else
                       }); // db.query
                     } // else
-                  }); // pool.connect
-                } else {
-                  res.sendStatus(401);
-                }
-              }
-              });
+                   // pool.connect
             }
+              });
+            // } else {
+            //   res.sendStatus( 403 );
+            // }
           });
 
 
