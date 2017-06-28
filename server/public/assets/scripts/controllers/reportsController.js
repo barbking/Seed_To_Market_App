bd7994238e4d5b33a1135e7cb7a416ecd34fd63f
Barb King <barbgross@aol.com>
2017-06-28 11:43:49 -0500

    csv modal

--- server/public/assets/scripts/controllers/reportsController.js
+++ server/public/assets/scripts/controllers/reportsController.js
@@ -1,4 +1,4 @@
-myApp.controller('ReportsController', ['$http', '$location', 'reportsService', function($http, $location, reportsService) {
+myApp.controller('ReportsController', ['$http', '$location','$uibModal','$log','reportsService', function($http, $location,$uibModal,$log,reportsService) {
   var vm = this;
 
   console.log ('in ReportsController');
@@ -8,4 +8,40 @@ myApp.controller('ReportsController', ['$http', '$location', 'reportsService', f
 
   console.log('ReportsController reports-->',vm.reports);
 
+  //open csv modal
+  vm.open = function ( size, parentSelector ) {
+    var parentElem = parentSelector ?
+      angular.element($document[0].querySelector('.csv-download-modal' + parentSelector)) : undefined;
+    var modalInstance = $uibModal.open({
+      animation: vm.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'csvDownloadModalContent.html',
+      controller: 'csvDownloadModalInstanceCtrl',
+      controllerAs: 'cdmic',
+      size: size,
+      appendTo: parentElem,
+      resolve: {
+
+      }
+    }); // end modalInstance
+  }; // end newActivity
+
 }]);
+
+//controller for PLANT modal
+myApp.controller( 'csvDownloadModalInstanceCtrl', [ '$uibModalInstance', '$uibModal', '$log', function ( $uibModalInstance, $uibModal, $log) {
+  var vm = this;
+
+  console.log( 'csvDownload in modal:');
+
+  vm.sendDates = function(){
+    var itemToSend = {
+      date_from: vm.date_from,
+      date_to: vm.date_to
+    };
+    reportsService.sendDates(itemToSend);
+    console.log(itemToSend);
+    $uibModalInstance.close();
+   };//end sendDates
+  }]);//end of addPlantModalInstanceCtrl controller
